d592abb32e54bf9afa10890956afdaa9285cbbf5
Bianca <biancalway@gmail.com>
2020-06-14 16:17:10 -0300

    Modificaçao de digitos validos e operadores validos para constantes. Metodo privado adicionado para melhorar a compreensao do uso dos operadores validos.

--- scanner.rb
+++ scanner.rb
@@ -1,14 +1,14 @@
 class Scanner
+    VALID_DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    VALID_OPERATORS = ["+", "-"]
+    
     def tokenizer(values)
         values = values.gsub(" ", "")
-        valid_digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        valid_operators = ["+", "-"]
-        valid_tokens = valid_digits + valid_operators
         i = 0
         while i < values.size
-            if valid_digits.include?(values[i])
+            if VALID_DIGITS.include?(values[i])
                 nil
-            elsif valid_operators.include?(values[i]) && valid_digits.include?(values[i-1]) && valid_digits.include?(values[i+1])
+            elsif is_valid_operator_use?(values, i)
                 nil
             else
                 raise "Unexpected value found!"
@@ -16,5 +16,13 @@ class Scanner
             i += 1
         end
         return nil
+    end
+
+    private
+   
+    def is_valid_operator_use?(values, i)
+        VALID_OPERATORS.include?(values[i]) && 
+        VALID_DIGITS.include?(values[i-1]) && 
+        VALID_DIGITS.include?(values[i+1])
     end
 end
