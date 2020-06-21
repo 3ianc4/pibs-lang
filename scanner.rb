048257ec40dbdd4f441faabc8c7931c0eaa2ee03
Bianca <biancalway@gmail.com>
2020-06-21 17:10:26 -0300

    Criado o array tokens. A funçao tokenizer passa a retornar tokens. Modificaçao da condiçao que detecta numeros, agora detecta grupos de numeros, nao mais unitarios.

--- scanner.rb
+++ scanner.rb
@@ -1,26 +1,31 @@
+require './token'
+
 class Scanner
     VALID_DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     VALID_OPERATORS = ["+", "-"]
     
     def tokenizer(values)
+        tokens = []
         values = values.gsub(" ", "")
-        i = 0
-        while i < values.size
-            if VALID_DIGITS.include?(values[i])
-                nil
-            elsif is_valid_operator_use?(values, i)
+        pointer = 0
+        while pointer < values.size
+            if values.match(/\d+/)
+                integer_tokens = values.scan(/\d+/)
+                tokens << Token.new("integer", integer_tokens.first.to_i)
+                pointer += integer_tokens.first.size
+            elsif is_valid_operator_use?(values, pointer)
+                pointer += 1
                 nil
             else
                 raise "Unexpected value found!"
             end
-            i += 1
         end
-        return nil
+        return tokens
     end
 
     private
    
-    def is_valid_operator_use?(values, i)
-        VALID_OPERATORS.include?(values[i])
+    def is_valid_operator_use?(values, pointer)
+        VALID_OPERATORS.include?(values[pointer])
     end
 end
