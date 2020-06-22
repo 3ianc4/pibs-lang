109e46a5f26e0ce03ecf1e885b65b88adde043bc
Bianca <biancalway@gmail.com>
2020-06-22 09:24:22 -0300

    Refatoraçao do codigo. Modificacao da variavel pointer para inclusao da coluna na mensagem de erro. Inserçao de constantes para representar as expressoes regulares.

--- scanner.rb
+++ scanner.rb
@@ -1,30 +1,42 @@
 require './token'
 
+VALID_INTEGERS = /^\d+/
+VALID_OPERATORS = /^\+|^\-/
+$pointer = 0
+
 class Scanner
     def tokenizer(values)
         tokens = []
         values = values.gsub(" ", "")
-        pointer = 0
+        #pointer = 0
         while !values.empty?
-            if values.match(/^\d+/)
-                integer_tokens = values.scan(/^\d+/)
+            if is_valid_integer?(values)
+                integer_tokens = values.scan(VALID_INTEGERS)
                 tokens << Token.new("integer", integer_tokens.first.to_i)
-                pointer += integer_tokens.first.size
-                values = values.sub(/^\d+/, "")
-            elsif values.match(/^\+|^\-/)
-                operator_tokens = values.scan(/^\+|^\-/)
+                $pointer += integer_tokens.first.size
+                values = values.sub(VALID_INTEGERS, "")
+            elsif is_valid_operator?(values)
+                operator_tokens = values.scan(VALID_OPERATORS)
                 tokens << Token.new("operator", operator_tokens.first)
-                pointer += 1
-                values = values.sub(/^\+|^\-/, "")
+                $pointer += 1
+                values = values.sub(VALID_OPERATORS, "")
             else
-                raise "Unexpected value found!"
+                raise "Unexpected value found at the #{$pointer} value!"
             end
         end
         return tokens
     end
 
     private
-   
+    
+    def is_valid_integer?(values)
+        values.match(VALID_INTEGERS)
+    end
+
+    def is_valid_operator?(values)
+        values.match(VALID_OPERATORS)
+    end
+
     def is_valid_operator_use?(values, pointer)
         VALID_OPERATORS.include?(values[pointer])
     end
