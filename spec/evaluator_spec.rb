d689c4d92c44c4b164c096b57076341c31193d06
Bianca <biancalway@gmail.com>
2020-07-04 11:48:25 -0300

    Adicionados testes que avaliam operaçoes complexas e operaçao que inicia com operador

--- spec/evaluator_spec.rb
+++ spec/evaluator_spec.rb
@@ -15,4 +15,24 @@ RSpec.describe Evaluator, "#evaluate" do
         result = evaluator.evaluate(tokens)
         expect(result).to eq 2
     end
+
+    it "evaluates complex sum operation" do
+        evaluator = Evaluator.new
+        tokens = [ 
+            Token.new("integer", 1), 
+            Token.new("operator", "+"), 
+            Token.new("integer", 1), 
+            Token.new("operator", "+"), 
+            Token.new("integer", 1)
+        ]
+        result = evaluator.evaluate(tokens)
+        expect(result).to eq 3
+    end
+
+    it "evaluates operation with operators in first position" do
+        evaluator = Evaluator.new
+        tokens = [Token.new("operator", "+"), Token.new("integer", 1)]
+        result = evaluator.evaluate(tokens)
+        expect(result).to eq 1
+    end
 end
