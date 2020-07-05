1d246599d2ef3cf153be698704e94e1b4e8090e2
Bianca <biancalway@gmail.com>
2020-07-05 14:42:39 -0300

    Criada a classe interpreter para ler o programa usando o scanner e o evaluator

--- interpreter.rb
+++ interpreter.rb
@@ -1,2 +1,20 @@
+require './scanner'
+require './evaluator'
+
 class Interpreter
+    def initialize(program)
+        @program = program
+        @scanner = Scanner.new
+        @evaluator = Evaluator.new
+        @lines = File.read(@program).split
+        @current_line = 0
+    end
+
+    def read_line
+        current_line_content = @lines[@current_line]
+        tokens = @scanner.tokenizer(current_line_content)
+        result = @evaluator.evaluate(tokens)
+        @current_line += 1
+        return result
+    end
 end
---
+++ spec/interpeter_spec.rb
@@ -0,0 +1,16 @@
+require './interpreter'
+
+RSpec.describe Interpreter, "#read_line" do
+    it "read line and return the result" do
+        interpreter = Interpreter.new("./spec/simple_expression.bibs")
+        result = interpreter.read_line()
+        expect(result).to eq 1
+    end
+    it "read multiple lines and return the result" do
+        interpreter = Interpreter.new("./spec/multiple_expressions.bibs")
+        first_line_result = interpreter.read_line()
+        expect(first_line_result).to eq 1
+        second_line_result = interpreter.read_line()
+        expect(second_line_result).to eq 2
+    end
+end
