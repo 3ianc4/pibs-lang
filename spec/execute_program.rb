d6e677a5c474146519f1e7cf8888c20937c8ff53
Bianca <biancalway@gmail.com>
2020-07-23 16:01:22 -0300

    Bibs is now Pibs.

--- spec/execute_program.rb
+++ spec/execute_program.rb
@@ -1,3 +1,3 @@
 require './interpreter'
-interpreter = Interpreter.new("spec/multiple_expressions.bibs")
+interpreter = Interpreter.new("spec/multiple_expressions.pibs")
 interpreter.execute_program
